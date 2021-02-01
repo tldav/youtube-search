da1de4b17c9aededfb2cc8b3d8ac40168042698b
Neverender <tldavis09@yahoo.com>
2021-02-01 09:18:52 -0600

    finish styling of video list and items

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./VideoItem.css"
 
 const VideoItem = ({video}) => {
     return (
-        <div className="item">
+        <div className="video-item item">
             <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title}/>
             <div className="content">
                 <div className="header">ID: {video.id.videoId}, title: {video.snippet.title}</div>
