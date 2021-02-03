731e9f3c0248534aec7d8946c9aaa41a7af8fe46
Neverender <tldavis09@yahoo.com>
2021-02-02 21:40:22 -0600

    styled selected video and video list into columns

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,9 +27,19 @@ class App extends Component {
         return ( 
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit}/>
-                <VideoDetail video={this.state.selectedVideo} />
 
-                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/> 
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                        <VideoDetail video={this.state.selectedVideo} />
+
+                        </div>
+                        <div className="five wide column">
+                        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/> 
+
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -10,7 +10,7 @@ const VideoDetail = ({video}) => {
     return ( 
         <div>
             <div className="ui embed">
-                <iframe src={videoUrl} frameborder="0"></iframe>
+                <iframe title={video.snippet.title} src={videoUrl} frameborder="0"></iframe>
             </div>
             <div className="ui segment"> 
                 <h4 className="ui header">{video.snippet.title}</h4>
