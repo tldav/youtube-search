73a814b89b81ddbaafcd8d2d208075f48444ff50
Neverender <tldavis09@yahoo.com>
2021-02-03 20:05:28 -0600

    Add env variables

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const KEY = "AIzaSyBlN5QYpoPysGfT1E_BuQQLoG0r6XhUVJw";
+const KEY = process.env.REACT_APP_API_KEY;
 
 export default axios.create({
 	baseURL: "https://www.googleapis.com/youtube/v3",
